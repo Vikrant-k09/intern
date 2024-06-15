0ddde11c3553103c8eaca9e55f4340192abb7a96
Vikrant <Vik.t.905@gmail.com>
2024-06-15 13:19:43 +0530

    Responsive

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -5,31 +5,27 @@ const Booking = ({ bookingId, category, sessionTime, sessionDate, duration, stat
     <div className="bg-white mt-20 w-full h-full">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="bg-white shadow-sm rounded-3xl p-8">
-          <div className="text-center text-2xl font-bold mb-8   ">Booking Details</div>
+          <div className="text-center text-2xl font-bold mb-8">Booking Details</div>
 
-          <div className="grid grid-cols-5 gap-4 items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 items-center">
             <div className="flex flex-col items-center">
-
-
               <div className="text-center">
-                <img src="/assets/Bookingid.png" className="rounded-full w-16 h-16" />
+                <img src="/assets/Bookingid.png" className="rounded-full w-16 h-16" alt="Booking ID" />
                 <div className="text-gray-700">Booking ID</div>
                 <div className="text-black font-bold">{bookingId}</div>
               </div>
             </div>
 
-
-
             <div className="flex flex-col items-center">
-            <img src="/assets/category.png" className="rounded-full w-16 h-16" />
-
+              <img src="/assets/category.png" className="rounded-full w-16 h-16" alt="Category" />
               <div className="text-center">
                 <div className="text-gray-700">Consultation Category</div>
                 <div className="text-black font-bold">{category}</div>
               </div>
             </div>
-            <div className="flex flex-col items-center mt-6">
-            <img src="/assets/20.png" className="rounded-full w-16 h-16" />
+
+            <div className="flex flex-col items-center mt-6 md:mt-0">
+              <img src="/assets/20.png" className="rounded-full w-16 h-16" alt="Session Time & Date" />
               <div className="text-center">
                 <div className="text-gray-700">Session Time & Date</div>
                 <div className="text-black font-bold">
@@ -38,8 +34,9 @@ const Booking = ({ bookingId, category, sessionTime, sessionDate, duration, stat
                 <div className="text-gray-500">{duration}</div>
               </div>
             </div>
+
             <div className="flex flex-col items-center">
-            <img src="/assets/17.png" className="rounded-full w-16 h-16" />
+              <img src="/assets/17.png" className="rounded-full w-16 h-16" alt="Booking Status" />
               <div className="text-center">
                 <div className="text-gray-700">Booking Status</div>
                 <div className={`font-bold ${status === "Confirmed" ? "text-blue-500" : "text-red-500"}`}>
@@ -47,10 +44,10 @@ const Booking = ({ bookingId, category, sessionTime, sessionDate, duration, stat
                 </div>
               </div>
             </div>
-            <div className="flex flex-col items-center">
 
+            <div className="flex flex-col items-center">
               <button className="px-6 py-2 bg-red-500 text-white rounded-full">
-              <a href="https://www.google.com">Join Session</a>
+                <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Join Session</a>
               </button>
             </div>
           </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import {
   CheckCircleIcon,
   CalendarIcon,
   CurrencyRupeeIcon,
-  ClockIcon
+  ClockIcon,
 } from "@heroicons/react/24/outline";
 import Calendar from "./Calendar";
 
@@ -17,95 +17,97 @@ const Hero = () => {
 
   return (
     <div>
-      <section className="bg-white  mt-20 w-full mb-6">
-        <div className=" mx-auto px-4 sm:px-6 lg:px-8">
+      <section className="bg-white mt-20 w-full mb-6">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
           <div className="bg-white shadow-md-pink rounded-2xl p-8 flex flex-col">
             {/* Image */}
-            <span className="flex flex-row w-full ">
-              <div className="mb-4 md:mb-0 md:mr-6">
+            <span className="flex flex-col md:flex-row w-full">
+              <div className="mb-4 md:mb-0 md:mr-6 flex justify-center">
                 <img
-                  className="rounded-full md:mx-0 max-md:w-[100px] h-[100px]"
+                  className="rounded-full w-[100px] h-[100px]"
                   src="/assets/heroavatar.png"
                   alt="Ridhima Sen"
                 />
               </div>
 
-              <div className="flex flex-row ml-8">
+              <div className="flex flex-col md:flex-row w-full">
                 {/* Name */}
-                <div className="md:w-2/3">
-                  <h2 className="text-2xl font-bold text-gray-900 text-center md:text-left mt-2">
+                <div className="md:w-2/3 text-center md:text-left">
+                  <h2 className="text-2xl font-bold text-gray-900 mt-2">
                     Ridhima Sen
                   </h2>
                   <p className="text-gray-600 mb-3">Startup Consultant</p>
                   <ul className="text-gray-600 mb-4">
-                    <li className="flex items-center py-1">
+                    <li className="flex items-center justify-center md:justify-start py-1">
                       <CheckCircleIcon className="h-6 w-6 text-gray-500 mr-1" />
                       <span className="ml-2">Qualified in startup Consulting</span>
                     </li>
-                    <li className="flex items-center py-1">
+                    <li className="flex items-center justify-center md:justify-start py-1">
                       <CalendarIcon className="h-6 w-6 text-gray-500 mr-1" />
                       <span className="ml-2">20 Years of experience in consulting</span>
                     </li>
-                    <li className="flex items-center py-1">
+                    <li className="flex items-center justify-center md:justify-start py-1">
                       <CreditCardIcon className="h-6 w-6 text-gray-500 mr-1" />
                       <span className="ml-2">Consultation fee ₹1000</span>
                     </li>
                   </ul>
                 </div>
 
-                <div className="h-full border-r border-black"></div>
+                <div className="h-full hidden md:block border-r border-black"></div>
 
-                <div className="mt-4 ml-16 w-[70%]">
+                <div className="mt-4 md:mt-0 md:ml-16 w-full md:w-[70%] text-center md:text-left">
                   <h2 className="text-2xl font-semibold text-gray-800 mb-8">
                     Consultation Prices
                   </h2>
-                  <div className="flex flex-row gap-4">
-                    <p className="text-gray-600 flex flex-row">
+                  <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start">
+                    <p className="text-gray-600 flex items-center justify-center md:justify-start">
                       <CurrencyRupeeIcon className="h-8 w-8 text-gray-500 mr-1" />
                       <span className="ml-1">30 min session: ₹1000</span>
                     </p>
-                    <p className="text-gray-600 flex flex-row">
+                    <p className="text-gray-600 flex items-center justify-center md:justify-start">
                       <CurrencyRupeeIcon className="h-8 w-8 text-gray-500 mr-1" />
                       <span className="ml-1">01 hr session: ₹1500</span>
                     </p>
-                    <p className="text-gray-600 flex flex-row">
+                    <p className="text-gray-600 flex items-center justify-center md:justify-start">
                       <ClockIcon className="h-8 w-8 text-gray-500 mr-1" />
                       <span className="ml-1">Availability: Mon-Sat</span>
                     </p>
                   </div>
-                  <button
-                    className="px-6 py-2 bg-red-500 text-white rounded-full mt-4"
-                    onClick={handleBookNowClick}
-                  >
-                    Book Now
-                  </button>
+                  <div className="flex justify-center md:justify-start">
+                    <button
+                      className="px-6 py-2 bg-red-500 text-white rounded-full mt-4"
+                      onClick={handleBookNowClick}
+                    >
+                      Book Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </span>
 
             {/* SKILLS */}
-            <span>
-              <div className="mt-4">
-                <h3 className="text-3xl font-semibold text-gray-800 mb-5 mt-5">Skills</h3>
-                <div className="flex flex-wrap mt-2 gap-5">
-                  <span className="bg-red-100 text-red-800 text-lg font-medium mr-2 px-2.5 py-0.5 rounded-xl">
-                    Marketing Consulting
-                  </span>
-                  <span className="bg-red-100 text-red-800 text-lg font-medium mr-2 px-2.5 py-0.5 rounded-xl">
-                    Accounts Consulting
-                  </span>
-                  <span className="bg-red-100 text-red-800 text-lg font-medium mr-2 px-2.5 py-0.5 rounded-xl">
-                    Business Consulting
-                  </span>
-                  <span className="bg-red-100 text-red-800 text-lg font-medium mr-2 px-2.5 py-0.5 rounded-xl">
-                    Business Consulting
-                  </span>
-                  <span className="bg-red-100 text-red-800 text-lg font-medium mr-2 px-2.5 py-0.5 rounded-xl">
-                    Business Consulting
-                  </span>
-                </div>
+            <div className="mt-4">
+              <h3 className="text-3xl font-semibold text-gray-800 mb-5 mt-5">
+                Skills
+              </h3>
+              <div className="flex flex-wrap gap-5 justify-center md:justify-start">
+                <span className="bg-red-100 text-red-800 text-lg font-medium px-2.5 py-0.5 rounded-xl">
+                  Marketing Consulting
+                </span>
+                <span className="bg-red-100 text-red-800 text-lg font-medium px-2.5 py-0.5 rounded-xl">
+                  Accounts Consulting
+                </span>
+                <span className="bg-red-100 text-red-800 text-lg font-medium px-2.5 py-0.5 rounded-xl">
+                  Business Consulting
+                </span>
+                <span className="bg-red-100 text-red-800 text-lg font-medium px-2.5 py-0.5 rounded-xl">
+                  Business Consulting
+                </span>
+                <span className="bg-red-100 text-red-800 text-lg font-medium px-2.5 py-0.5 rounded-xl">
+                  Business Consulting
+                </span>
               </div>
-            </span>
+            </div>
           </div>
         </div>
       </section>
